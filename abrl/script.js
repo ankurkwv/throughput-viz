52f390ffe80d9330b01b8870d341fd480a3f7d3f
Ankur Kumar <me@ankuk.com>
2023-09-12 16:50:50 -0400

    adjust abrl for extra enqueue step

--- abrl/script.js
+++ abrl/script.js
@@ -218,7 +218,7 @@
                     queueDuration // How long it would take to service if 0 messages in queue
                     + (messagesBeingServiced * servicingDelayMs) // How much to delay it because of queued messages
                     - lastQueuedMessageProgress; // How much to speed it up because the next message's progress already
-                    - 100;
+                    - 200;
       anime({
           targets: msg,
           delay: 0,
@@ -263,7 +263,7 @@
             { 
                 translateX: POSITIONS['QUEUE-4_START'].x, 
                 translateY: POSITIONS['QUEUE-4_START'].y, 
-                duration: 500
+                duration: 100
             }
         ],
         easing: "linear", 
